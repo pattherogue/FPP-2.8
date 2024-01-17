2a063a93e73fb97b9ff559917960f87134a4f0f1
Paolo Gomez <peweegardner248@gmail.com>
2024-01-17 09:14:21 -0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+/*
+const friend = "BRUTUS";
+const shiftValue = 3;
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
+*/
+
+//Create a function named encryptLetter that takes a letter and a shift value as parameters. This function should return the encrypted version of the letter.
+/*
+- Inside the function, find the index of the letter in the alphabet.
+- Add the shift value to this index.
+- Use the modulus operator to ensure wrapping around the alphabet if necessary.
+- Return the encrypted letter.
+*/
+
+
+function encryptLetter(letter, shift) {
+
+}
